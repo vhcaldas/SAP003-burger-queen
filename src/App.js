6a7c5293d8cba0444a0c106d9166d248a2387460
vhcaldas <vivianhcaldas@gmail.com>
2019-12-22 20:58:49 -0300

    files organized. Added font to Restaurant.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Restaurant from './Pages/Restaurant';
 
+
 function App() { 
   return (
     <>
--- src/Pages/Restaurant.js
+++ src/Pages/Restaurant.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { db } from '../firebaseUtil.js';
+import { StyleSheet, css } from 'aphrodite';
+import { db } from '../Utils/firebaseUtil.js';
 import Card from '../Components/Card.js';
 import Header from '../Components/Header/Header.js';
 
@@ -20,17 +21,66 @@ const Restaurant = () => {
         }, []);
 
     return (
-        <main>
+        <div>
             <Header/>
-            
-            {menu.map(menuItem => 
-                <Card 
-                name={menuItem.name} 
-                price={menuItem.price} 
-                handleClick={() => console.log(menuItem)}/>
-            )}
-        </main>
+            <main className={css(styles.main)}>
+                <section className={css(styles.options)}>
+                    <h1>CAFÉ DA MANHÃ:</h1>            
+                        {menu.map(menuItem => { 
+                            return menuItem.breakfast ? (
+                                <Card 
+                                name={menuItem.name} 
+                                price={menuItem.price} 
+                                handleClick={() => console.log(menuItem)}/>
+                            ) : (
+                            false
+                            );
+                        })}
+                    <h1>DEMAIS OPÇÕES:</h1>
+                        <h2>Hambúrgueres:</h2>
+                            {menu.map(menuItem => { 
+                                return menuItem.burguer ? (
+                                    <Card 
+                                    name={menuItem.name} 
+                                    price={menuItem.price} 
+                                    handleClick={() => console.log(menuItem)}/>
+                                ) : (
+                                false
+                                );
+                            })}
+                        <h2>Acompanhamentos:</h2>
+                            {menu.map(menuItem => { 
+                                return menuItem.sidedish ? (
+                                    <Card 
+                                    name={menuItem.name} 
+                                    price={menuItem.price} 
+                                    handleClick={() => console.log(menuItem)}/>
+                                ) : (
+                                false
+                                );
+                            })}
+                        <h2>Bebidas:</h2>
+                            {menu.map(menuItem => { 
+                                return menuItem.beverage ? (
+                                    <Card 
+                                    name={menuItem.name} 
+                                    price={menuItem.price} 
+                                    handleClick={() => console.log(menuItem)}/>
+                                ) : (
+                                false
+                                );
+                            })}
+                </section>
+            </main>
+        </div>
     )
 }
+
+const styles = StyleSheet.create({
+    main:{
+        fontFamily: ['Montserrat Alternates', 'sans-serif'],
+        src: "url('https://fonts.googleapis.com/css?family=Montserrat+Alternates&display=swap')"
+    },
+})
 
 export default Restaurant
