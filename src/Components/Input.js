62918cfc608004c9cbb88e11386d3d940c41c675
vhcaldas <vivianhcaldas@gmail.com>
2020-01-06 17:57:08 -0300

    Finished CSS input and option buttons

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
 
 const Input = (props) => {
     return (
         <input 
             placeholder={props.placeholder} 
-            className='input'
+            className={css(styles.input)}
             type={props.type}
             value={props.value}
             onChange={props.handleChange}
@@ -12,5 +13,18 @@ const Input = (props) => {
         </input> 
     )
 }
+
+const styles = StyleSheet.create({
+
+    input:{
+        borderRadius: '2vw',
+        borderColor: '#FFB800',
+        width: '25vw',
+        height: '2vh',
+        margin: '0vw 3vw 1vw 3vw',
+        padding: '1vw'
+    }
+
+})
 
 export default Input;
---
+++ src/Components/OptButton.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
+function OptButton(props) {
+    return (
+        <button onClick={props.handleClick} id={props.id} className={css(styles.button)}>
+            <p>{props.name}</p>
+            <p>R$ {props.price}</p>
+        </button>
+    )
+}
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: '#FFB800',
+        color: '#0C0804',
+        width: '15vw',
+        height: 'auto',
+        margin: '2vw 3vw 1vw 3vw',
+        fontSize: '0.8rem',
+        borderRadius: '2vw',
+        fontWeight: 'bold',
+        border: 'none',
+    }
+})
+
+export default OptButton;
